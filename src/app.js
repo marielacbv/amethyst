b3f4f25b4ccfbcd3c90b9b428e7538ea48176ebb
marielacbv <marielacbv@gmail.com>
2022-12-18 09:57:51 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -45,10 +45,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-// Open Weather api
-let city = "Paris";
-let apiKey = "cfb51fc5dbb4cbe2d0268e3189a05c67";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "cfb51fc5dbb4cbe2d0268e3189a05c67";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
