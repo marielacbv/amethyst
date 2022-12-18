f06e8dabd17e5d1cb16650a26b416a0041f66134
marielacbv <marielacbv@gmail.com>
2022-12-18 04:18:20 -0400

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,22 @@
+// Functions
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  console.log(response.data.wind.speed);
+}
+
+// Open Weather api
+let apiKey = "cfb51fc5dbb4cbe2d0268e3189a05c67";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
